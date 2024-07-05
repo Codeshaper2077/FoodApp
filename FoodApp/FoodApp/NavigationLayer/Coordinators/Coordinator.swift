7b6bb707314aabc761ec648f17fe7d2cf2678a0f
codeshaper2077 <dartWayne1998android@gmail.com>
2024-07-06 00:10:28 +0500

    Added coordinators protocol

---
+++ FoodApp/FoodApp/NavigationLayer/Coordinators/Coordinator.swift
@@ -0,0 +1,39 @@
+//
+//  Coordinator.swift
+//  FoodApp
+//
+//  Created by user on 05.07.2024.
+//
+
+import UIKit
+
+enum CoordinatorType {
+    case app
+    case onboarding
+    case home
+    case order
+    case list
+    case profile
+}
+
+protocol CoordinatorProtocol: AnyObject {
+    var childCoordinators: [CoordinatorProtocol] { get set }
+    var type: CoordinatorType { get }
+    var navigationController: UINavigationController? { get set }
+    var finishDelegate: CoordinatorFinishDelegate? { get set }
+    func start()
+    func finish()
+}
+
+protocol CoordinatorFinishDelegate: AnyObject {
+    func coordinatorDidFinish(childCoordinators: CoordinatorProtocol)
+}
+
+extension CoordinatorProtocol {
+    func addChildCoordinator(_ childCoordinator: CoordinatorProtocol) {
+        childCoordinators.append(childCoordinator)
+    }
+    func removeChildCoordinator(_ childCoordinator: CoordinatorProtocol) {
+        childCoordinators = childCoordinators.filter { $0 !== childCoordinator }
+    }
+}
